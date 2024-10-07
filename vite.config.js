107ffb1951839dc59d2c5903bea2a92920988744
Thiago Higuchi <thiago.higuchi@gmail.com>
2024-10-07 00:26:53 -0300

    Added deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ninjaone-client/', // Replace with your GitHub repository name
   plugins: [react()],
   optimizeDeps: {
     include: ['i18next-http-backend'],
