a65e34d04164c0a528d34918b4f7567f912645e9
Thiago Higuchi <thiago.higuchi@gmail.com>
2024-10-07 02:14:06 -0300

    Removed VITE base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ninjaone-client/', // Replace with your GitHub repository name
+  base: './', // Replace with your GitHub repository name
   optimizeDeps: {
     include: ['i18next-http-backend'],
   },
