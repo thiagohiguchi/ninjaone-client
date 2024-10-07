f4f754ff076a7fa32a47bdce5032f51970e54ee5
Thiago Higuchi <thiago.higuchi@gmail.com>
2024-10-07 01:42:35 -0300

    Additions settings

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ninjaone-client/', // Replace with your GitHub repository name
   plugins: [react()],
+  base: '/ninjaone-client/', // Replace with your GitHub repository name
   optimizeDeps: {
     include: ['i18next-http-backend'],
   },
